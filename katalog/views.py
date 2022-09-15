458c09fea224f59e2ebaf810cd302984fa02d5c1
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2022-09-15 09:48:14 +0700

    menambah folder katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'name' : 'Failasuf Indi Marsendy',
+    'npm' : '2106750364',
+    'list_katalog': data_katalog,
+}
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html",context)
+
